92ffbdfedf5f662d61c85b540b1d01e09a28d8a9
Nguyen Nguyen Khang[khangnguyen] <khangnguyen@nexondv.com>
2023-12-06 16:17:27 +0700

    update 6/12

---
+++ WebApplication1/Connection/AccessControlAllowOriginModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+
+public class AccessControlAllowOriginModule : IHttpModule
+{
+    public void Dispose()
+    {
+    }
+
+    public void Init(HttpApplication context)
+    {
+        context.PreSendRequestHeaders += context_PreSendRequestHeaders;
+    }
+
+    void context_PreSendRequestHeaders(object sender, EventArgs e)
+    {
+        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type");
+    }
+}
